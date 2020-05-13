c56a67db2b9cdbce347c47688975c75279f12126
Ji Shan <uug54332017@gmail.com>
2020-05-13 13:50:55 +1000

    product model test added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :farmer
   has_one_attached :picture
+
+  validates :title, presence: true, length: {in: 1..15}
+  validates :rank, presence: true
+  validates :description, presence: true, length: {in: 1..30}
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 10 }
+  validates :farmer_id, presence: true
+
+  def display_currency
+    '$' + sprintf('%.2f', (self.price).to_s)
+  end
+  
 end
