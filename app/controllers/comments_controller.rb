549733bf73bb00bf3d21d3a712030ffa62c4ad67
Ji Shan <uug54332017@gmail.com>
2020-05-22 23:20:43 +1000

    ability.rbc for comment updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+load_and_authorize_resource
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.create(comment_params)
