7ae69524a2a2b81c9039771b01178fb7e828396d
Ji Shan <uug54332017@gmail.com>
2020-05-29 16:34:24 +1000

    import leaflet.css in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
+import "leaflet/dist/leaflet.css"
 import "../stylesheets/application"
 
 
